d6dd0da0f8f6105455de85d764b2bdc59015a2dc
sh1mj1 <7wlgns@gmail.com>
2024-11-07 22:28:12 +0900

    feat(ArticleAdapter): implement PagingDataAdapter

--- basic/start/app/src/main/java/com/example/android/codelabs/paging/ui/ArticleAdapter.kt
+++ basic/start/app/src/main/java/com/example/android/codelabs/paging/ui/ArticleAdapter.kt
@@ -18,15 +18,15 @@ package com.example.android.codelabs.paging.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import com.example.android.codelabs.paging.data.Article
 import com.example.android.codelabs.paging.databinding.ArticleViewholderBinding
 
 /**
  * Adapter for an [Article] [List].
  */
-class ArticleAdapter : ListAdapter<Article, ArticleViewHolder>(ARTICLE_DIFF_CALLBACK) {
+class ArticleAdapter : PagingDataAdapter<Article, ArticleViewHolder>(ARTICLE_DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder =
         ArticleViewHolder(
