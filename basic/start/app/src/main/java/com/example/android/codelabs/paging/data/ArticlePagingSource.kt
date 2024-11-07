504268533fc994ef1a6194c309f4ecfff247f4b7
sh1mj1 <7wlgns@gmail.com>
2024-11-07 22:00:20 +0900

    feat(ArticlePagingSource): getRefreshKey

--- basic/start/app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
+++ basic/start/app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
@@ -32,8 +32,13 @@ class ArticlePagingSource : PagingSource<Int, Article>() {
         )
     }
 
+    // 무효화 이후에 다음 페이징 소스의 첫 로드에서 사용되는 키를 반환한다.
     override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
-        TODO("Not yet implemented")
+        // 우리는  anchor position 에서 가장 가까운 아이템을 찾아서 그것의 id - (state.config.pageSize / 2) 를 반환한다. as a buffer
+        // anchor position: Most recently accessed index in the list, including placeholders.
+        val anchorPosition = state.anchorPosition ?: return null
+        val article: Article = state.closestItemToPosition(anchorPosition) ?: return null
+        return validKey(key = article.id - (state.config.pageSize / 2))
     }
 
     // 페이징 키가 [STARTING_KEY] 보다 작지 않도록 보장
@@ -82,6 +87,34 @@ private val firstArticleCreatedTime = LocalDateTime.now()
  * ensureValidKey() 메서드를 정의하여 이 작업을 수행합니다.
  * 이 함수는 페이징 키가 유효한지 확인합니다.
  *
+ * ---
+ *
+ * Next we need to implement getRefreshKey().
+ * This method is called when the Paging library needs to reload items for the UI because the data in its backing PagingSource has changed.
+ * This situation where the underlying data for a PagingSource has changed and needs to be updated in the UI is called invalidation.
+ * When invalidated, the Paging Library creates a new PagingSource to reload the data, and informs the UI by emitting new PagingData. We'll learn more about invalidation in a later section.
+ *
+ * When loading from a new PagingSource, getRefreshKey() is called to provide the key
+ * the new PagingSource should start loading with to make sure the user does not lose their current place in the list after the refresh.
+ *
+ * Invalidation in the paging library occurs for one of two reasons:
+ *
+ * You called refresh() on the PagingAdapter.
+ * You called invalidate() on the PagingSource.
+ *
+ * 반환된 키는 LoadParams argument 를 통해서 새로운 PagingSource의 load() 메서드에 전달됩니다.
+ * 무효화 이후에 아이템이 이동하는 것을 방지하기 위해, 반환된 키가 화면을 채울만큼의 아이템을 로드하는지 확인해야합니다.
+ * 이렇게 하면 새로운 아이템 세트가 무효화된 데이터에 있던 아이템을 포함할 가능성이 높아져 현재 스크롤 위치를 유지하는데 도움이 됩니다.
+ *
+ *
+ * 위 getRefreshKey 코드에서 PagingState.anchorPosition 을 사용했습니다.
+ * 만약 페이징 라이브러리가 더 많은 데이터를 어떻게 가져올 수 있는지 궁금하다면, 이것이 답입니다!
+ * UI가 PagingData에서 항목을 읽으려고 시도하면 특정 인덱스에서 읽으려고 시도합니다.
+ * 데이터가 읽히면 그 데이터가 UI에 표시됩니다. 그러나 데이터가 없으면 페이징 라이브러리는 데이터를 가져와 실패한 읽기 요청을 충족해야 한다는 것을 알게됩니다.
+ * 데이터가 성공적으로 읽힌 가장 최근 인덱스는 anchorPosition입니다.
+ * 새로 고칠 때, anchorPosition에 가장 가까운 Article의 키를 가져와서 로드 키로 사용합니다.
+ * 이렇게 하면 새로운 PagingSource에서 다시 로드를 시작할 때 이미 로드된 항목이 포함된 항목 세트가 포함되어 부드럽고 일관된 사용자 경험을 보장합니다.
+ * 이렇게 하면 새로운 PagingSource를 완전히 정의했습니다. 다음 단계는 UI에 연결하는 것입니다.
  *
  *
  *
