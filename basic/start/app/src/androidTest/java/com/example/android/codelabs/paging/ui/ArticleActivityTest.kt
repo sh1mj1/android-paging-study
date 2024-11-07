439339a55d67a56ef860d5a8e66cea52bc57e395
sh1mj1 <7wlgns@gmail.com>
2024-11-08 01:31:56 +0900

    test: default test 추가

---
+++ basic/start/app/src/androidTest/java/com/example/android/codelabs/paging/ui/ArticleActivityTest.kt
@@ -0,0 +1,13 @@
+package com.example.android.codelabs.paging.ui
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ArticleActivityTest {
+    @Test
+    fun defaultTest() {
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual)
+    }
+}
---
+++ basic/start/app/src/test/java/com/example/android/codelabs/paging/data/ArticleTest.kt
@@ -0,0 +1,13 @@
+package com.example.android.codelabs.paging.data
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ArticleTest {
+    @Test
+    fun defaultTest() {
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual)
+    }
+}
