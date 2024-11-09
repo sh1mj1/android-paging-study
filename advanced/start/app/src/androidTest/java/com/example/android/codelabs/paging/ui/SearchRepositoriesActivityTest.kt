67f00c99b0e0152cf21c9f5d052e35b90d88a300
sh1mj1 <7wlgns@gmail.com>
2024-11-10 02:09:56 +0900

    build: test 의존성 추가, 기본 테스트 추가

---
+++ advanced/start/app/src/androidTest/java/com/example/android/codelabs/paging/ui/SearchRepositoriesActivityTest.kt
@@ -0,0 +1,15 @@
+package com.example.android.codelabs.paging.ui
+
+import io.kotest.matchers.shouldBe
+import org.junit.Assert.*
+import org.junit.Test
+
+class SearchRepositoriesActivityTest {
+    @Test
+    fun defaultTest() {
+        val expected = 1
+        val actual = 1
+        assertEquals(expected, actual)
+        expected shouldBe actual
+    }
+}
---
+++ advanced/start/app/src/test/java/com/example/android/codelabs/paging/model/RepoTest.kt
@@ -0,0 +1,12 @@
+package com.example.android.codelabs.paging.model
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class RepoTest {
+    @Test
+    fun defaultTest() {
+        val a = 1
+        a shouldBe 1
+    }
+}
